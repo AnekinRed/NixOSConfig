e37ef2325c314f2599d4aac1b78830fa06caaaaf
AnekinRed <125562038+AnekinRed@users.noreply.github.com>
2025-08-15 20:41:01 +0200

    Initial Commit
    (shit is not working yet)

---
+++ flake.nix
@@ -0,0 +1,22 @@
+            {
+  inputs = {
+    # This is pointing to an unstable release.
+    # If you prefer a stable release instead, you can this to the latest number shown here: https://nixos.org/download
+    # i.e. nixos-24.11
+    # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+     zen-browser = {
+    url = "github:0xc000022070/zen-browser-flake";
+    # IMPORTANT: we're using "libgbm" and is only available in unstable so ensure
+    # to have it up-to-date or simply don't specify the nixpkgs input
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+ };
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    # NOTE: 'nixos' is the default hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
+
