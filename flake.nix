6047caee9ea7e2eab607577e3401d630ff8a81f5
AnekinRed <AnekinRed@gmail.com>
2025-08-17 22:10:55 +0200

    Version 1.0.6 I think it works

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
   outputs = inputs: {
 
-    nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
+    nixosConfigurations.AnekinRedsLaptop = inputs.nixpkgs.lib.nixosSystem {
       modules = [
         { nix.settings.experimental-features = ["nix-command" "flakes"]; }
         ./configuration.nix
